20dbb5c061f03696c8b967a91d310a970ea6852d
Yegor Kazantsev <wwwadmin@bk.ru>
2024-11-15 00:13:26 +0300

    move frontend to main repo

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
+        base: '',
+        root: 'src',
+        build: {
+            outDir: 'public'
+        },
         editor: {
             entry: resolve(__dirname, 'frontend/editor.js'),
             name: 'Editor',
             // the proper extensions will be added
             fileName: '/js/editor.js',
-        },
-        rollupOptions: {
-            input: {
-              edit: resolve(__dirname, 'index.html'),
-            },
         },
     },
 })
