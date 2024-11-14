cc8cd2f253d4a51ef30116631df0dcb2e56f2ef3
Yegor Kazantsev <wwwadmin@bk.ru>
2024-11-14 19:57:01 +0300

    move frontend to main repo

---
+++ vite.vercel.config.js
@@ -0,0 +1,24 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        base: '',
+        root: 'src',
+        build: {
+          outDir: '../public'
+        },
+        editor: {
+            entry: resolve(__dirname, 'editor.js'),
+            name: 'Editor',
+            // the proper extensions will be added
+            fileName: 'js/editor.js',
+        },
+        rollupOptions: {
+            input: {
+              edit: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
+})
