ce8d4963372858ec3814b4ed60c88f84b450e053
Yegor Kazantsev <wwwadmin@bk.ru>
2024-11-18 20:47:02 +0300

    terser

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 /* Vercel build config */
+import { terser } from 'rollup-plugin-terser'
+
 export default {
     publicDir: 'assets',
     root: 'frontend/',
     build: {
+        minify: 'terser',
         outDir: '../dist',
         file: 'main.js',
         format: 'iife',
-        name: 'MyBundle'
+        name: 'MyBundle',
+        rollupOptions: {
+            plugins: [
+                terser() // Use terser plugin
+            ],
+            input: 'editor.js', // Your entry point
+            output: {
+                file: 'editor.min.js', // Output file
+                format: 'es', // Output format
+                name: 'Editor' // Name of the global variable
+            }
+        }
     }
 }
